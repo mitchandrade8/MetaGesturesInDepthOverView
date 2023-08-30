f018b2596bdb16403a0a1222a103d9544485ba0f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-30 17:20:10 -0600

    Initial Commit

---
+++ MetaGesturesInDepthOverView/MetaGesturesInDepthOverViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaGesturesInDepthOverViewApp.swift
+//  MetaGesturesInDepthOverView
+//
+//  Created by Mitch Andrade on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaGesturesInDepthOverViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
